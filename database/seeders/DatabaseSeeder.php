0c1fe14f5272712e38d3d0294f86805ecaa4f338
BryanGK <bryangordonkrauss@gmail.com>
2021-07-23 08:55:05 -0700

    Adds DB seeding. Refactors routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam incidunt placeat molestias doloribus voluptatibus facere omnis rerum iure debitis. Nisi culpa obcaecati facilis accusamus, modi vel! Vel sequi quaerat asperiores?'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam incidunt placeat molestias doloribus voluptatibus facere omnis rerum iure debitis. Nisi culpa obcaecati facilis accusamus, modi vel! Vel sequi quaerat asperiores?'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam incidunt placeat molestias doloribus voluptatibus facere omnis rerum iure debitis. Nisi culpa obcaecati facilis accusamus, modi vel! Vel sequi quaerat asperiores?'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
+use App\Models\Category;
 
 
 /*
@@ -17,13 +18,22 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
+    // \Illuminate\Support\Facades\DB::listen(function ($query) {
+    //     logger($query->sql);
+    // });
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
