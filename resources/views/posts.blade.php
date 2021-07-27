fb5128f75830044a99c3ca5920467e1c88b6cea5
BryanGK <bryangordonkrauss@gmail.com>
2021-07-27 08:28:54 -0700

    adds frontend to site

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include ('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card :post="$posts[0]" />
+
+        {{-- <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div> --}}
+    </main>
+    {{-- @foreach ($posts as $post)
         <article>
             <h1>
                 <a href="/posts/{{ $post->slug }}">
@@ -13,6 +29,6 @@
                     {!! $post->excerpt !!}
             </div>
         </article>
-    @endforeach
+    @endforeach --}}
 
 </x-layout>
